14ccc1ecbfafb608f74743ec8a8525a968382fa5
DeevsDeevs <deevsgt15@gmail.com>
2024-05-17 10:10:10 -0300

    removed csv example from delta tooltip

--- examples/plugins/02_delta_tooltip.jl
+++ examples/plugins/02_delta_tooltip.jl
@@ -1,32 +1,52 @@
-# examples/plugins/02_delta_tooltip
+# examples/charts/06_candlesticks
 #
 # Run from the project root with the command
-# julia --project=. examples/plugins/02_delta_tooltip.jl
+# julia --project=. examples/charts/06_candlesticks.jl
 
 using Dates
-using NanoDates
+using CryptoAPIs
 using LightweightCharts
-using TimeArrays
+import LightweightCharts: randcolor
 
-timearray = TimeArray{NanoDate,Float64}()
-
-open(joinpath(@__DIR__, "..", "samples", "btcusdt.csv"), "r") do file
-    for line in eachline(file)
-        dt, v = split(line, "    ")
-        push!(timearray, (NanoDate(dt), parse(Float64, v)))
-    end
-end
-
-plt = lwc_layout(
-    lwc_panel(
-        lwc_line(
-            timearray;
-            label_name = "BTCUSDT",
-            line_type = LWC_STEP,
-            plugins = [lwc_delta_tooltip()],
-        ),
-    ),
-    sync = false,
+ohlc = CryptoAPIs.Binance.Spot.candle(;
+    symbol = "ETHUSDT",
+    interval = CryptoAPIs.Binance.Spot.Candle.d1,
+    startTime = DateTime("2021-01-01"),
+    endTime = DateTime("2023-09-27"),
+    limit = 1000,
 )
 
-lwc_show(plt)
+panel = lwc_panel(
+    lwc_candlestick(
+        map(
+            x -> LWCCandle(
+                x.openTime,
+                x.openPrice,
+                x.highPrice,
+                x.lowPrice,
+                x.closePrice;
+                color = randcolor(),
+                border_color = randcolor(),
+                wick_color = randcolor(),
+            ),
+            ohlc.result,
+        ),
+        label_name = "lwc_candlestick",
+        up_color = "#52a49a",
+        down_color = "#de5e57",
+        border_visible = false,
+        price_scale_id = LWC_LEFT,
+        plugins = [lwc_delta_tooltip()],
+    ),
+    lwc_histogram(
+        map(x -> x.openTime, ohlc.result),
+        map(x -> x.volume, ohlc.result);
+        label_name = "lwc_histogram",
+        base = -100.0,
+        color = "rgba(47, 112, 181, 0.5)",
+        price_scale_id = LWC_RIGHT,
+    ),
+    name = "ETHUSDT | Binance Spot",
+)
+
+lwc_show(panel)
