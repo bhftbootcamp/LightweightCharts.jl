3281cd9e17e20ab051e853b6dc83dd31ed5d46ef
AlexKlo <65765845+AlexKlo@users.noreply.github.com>
2024-09-06 21:18:48 +0400

    Add chart display support for WSL (#25)
    Co-authored-by: AlekseiKlo <alexeyklo@engee.cloud>

--- src/LightweightCharts.jl
+++ src/LightweightCharts.jl
@@ -362,14 +362,24 @@ function Serde.SerJson.ser_name(::Type{A}, ::Val{T}) where {A<:AbstractPluginSet
     return to_camelcase(T)
 end
 
+function is_wsl()
+    Sys.islinux() &&
+    isfile("/proc/sys/kernel/osrelease") &&
+    occursin(r"Microsoft|WSL"i, read("/proc/sys/kernel/osrelease", String))
+end
+
 function open_browser(url::String)
     if Sys.isapple()
         Base.run(`open $url`)
         true
+    elseif is_wsl()
+        path_for_wsl = chomp(read(`wslpath -w $url`, String))
+        run(`wslview $path_for_wsl`)
+        true
     elseif Sys.islinux()
         Base.run(`xdg-open $url`)
         true
-    elseif Sys.iswindows() || detectwsl()
+    elseif Sys.iswindows()
         Base.run(`powershell.exe Start "'$url'"`)
         true
     else
